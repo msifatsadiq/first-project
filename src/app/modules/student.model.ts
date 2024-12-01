ea200199e5a4985c8e44375175b52d1bab91dd69
msifatsadiq <msifatsadiq@gmail.com>
2024-12-01 12:22:53 +0600

    adding schema, route,service,controller etc

---
+++ src/app/modules/student.model.ts
@@ -0,0 +1,88 @@
+import { Schema, model, connect } from "mongoose";
+import {
+  Guardian,
+  localGuardian,
+  Student,
+  UserName,
+} from "./student/student.interface";
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+// 2. Create a Schema corresponding to the document interface.
+
+const localGuardianSchema = new Schema<localGuardian>({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+
+  gender: ["male", "female"],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: { type: String },
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: ["Active", "Blocked"],
+});
+
+// 3. Create a Model.
+export const StudentModel = model<Student>("Student", studentSchema);
